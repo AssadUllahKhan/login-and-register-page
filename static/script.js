80d1937029c03ec588016c3eff5748986df896d4
Assad Ullah Khan <121865741+AssadUllahKhan@users.noreply.github.com>
2023-08-28 20:41:56 +0500

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,18 @@
+// home file password toggle
+let password= document.getElementById("password")
+let eyeicon= document.getElementById("eyeicon")
+
+eyeicon.onclick = function(){
+    if(password.type == "password"){
+        password.type = "text";
+        eyeicon.src = "images/eye-open.png";
+    }
+    else{
+        password.type = "password";
+        eyeicon.src ="images/eye-close.png";
+    }
+}
+
+
+// register file
+// register file password toggle
